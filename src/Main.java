759ff0ce61f1994084c2b34367ecc0df019a3e78
bahae3 <bahaeddiine12@gmail.com>
2024-04-28 15:36:24 +0100

    Main file works well, click run to start the program.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import database.database.*;
-
 
 public class Main {
     public static void main(String[] args) {
-        Login l = new Login();
+        new Login();
     }
 }
