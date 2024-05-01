7573fb1ed053374fc16081ad46d3347465ba3480
bahae3 <bahaeddiine12@gmail.com>
2024-05-01 01:55:36 +0100

    Signup.java still needs to have another 2 fields for codeUser and estPersonnel, and the data aren't stocked in the db yet

--- src/Signup.java
+++ src/Signup.java
@@ -1,14 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class Signup extends JFrame {
     JPanel panel = new JPanel() {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.setColor(new Color(255, 255, 153));
+            g.setColor(new Color(237, 210, 133));
             g.fillRect(0, 0, getWidth(), getHeight());
         }
     };
@@ -25,32 +24,32 @@ public class Signup extends JFrame {
         JLabel prenom = new JLabel("Prénom:");
         prenom.setBounds(430, 90, 100, 25);
 
-        JTextField prenomTf = new JTextField();
-        prenomTf.setBounds(430, 120, 160, 25);
+        JTextField prenomData = new JTextField();
+        prenomData.setBounds(430, 120, 160, 25);
 
         JLabel nom = new JLabel("Nom:");
         nom.setBounds(610, 90, 100, 25);
 
-        JTextField nomTf = new JTextField();
-        nomTf.setBounds(610, 120, 160, 25);
+        JTextField nomData = new JTextField();
+        nomData.setBounds(610, 120, 160, 25);
 
         JLabel email = new JLabel("Email:");
         email.setBounds(430, 165, 100, 25);
 
-        JTextField emailTf = new JTextField();
-        emailTf.setBounds(430, 195, 340, 25);
+        JTextField emailData = new JTextField();
+        emailData.setBounds(430, 195, 340, 25);
 
         JLabel mdp = new JLabel("Mot de passe:");
         mdp.setBounds(430, 245, 140, 25);
 
-        JPasswordField mdpTf = new JPasswordField();
-        mdpTf.setBounds(430, 275, 340, 25);
+        JPasswordField mdpData = new JPasswordField();
+        mdpData.setBounds(430, 275, 340, 25);
 
         JLabel code = new JLabel("Code étudiant:");
         code.setBounds(430, 325, 140, 25);
 
-        JTextField codeTf = new JTextField();
-        codeTf.setBounds(430, 355, 340, 25);
+        JTextField codeData = new JTextField();
+        codeData.setBounds(430, 355, 340, 25);
 
         JButton signup = new JButton("S'inscrire");
         signup.setBounds(610, 405, 140, 30);
@@ -71,40 +70,34 @@ public class Signup extends JFrame {
         retour.setFont(new Font("Arial", Font.PLAIN, 18));
 
         this.panel.add(prenom);
-        this.panel.add(prenomTf);
+        this.panel.add(prenomData);
         this.panel.add(nom);
-        this.panel.add(nomTf);
+        this.panel.add(nomData);
         this.panel.add(email);
-        this.panel.add(emailTf);
+        this.panel.add(emailData);
         this.panel.add(mdp);
-        this.panel.add(mdpTf);
+        this.panel.add(mdpData);
         this.panel.add(code);
-        this.panel.add(codeTf);
+        this.panel.add(codeData);
         this.panel.add(signup);
         this.panel.add(retour);
 
         this.setContentPane(this.panel);
 
-        signup.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                // Retrieving username and password
-                String prenom = prenomTf.getText();
-                String nom = nomTf.getText();
-                String email = emailTf.getText();
-                String mdp = new String(mdpTf.getPassword());
-                Integer codeEtudiant = Integer.valueOf(codeTf.getText());
+        signup.addActionListener(evt -> {
+            // Retrieving username and password
+            String prenomUser = prenomData.getText();
+            String nomUser = nomData.getText();
+            String emailUser = emailData.getText();
+            String mdpUser = new String(mdpData.getPassword());
+            Integer codeUser = Integer.valueOf(codeData.getText());
 
-            }
         });
 
-        retour.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                // interface dyal sign up ghatmshi w ghatla3 dyal login
-                new Login().setVisible(true);
-                setVisible(false);
-            }
+        retour.addActionListener(evt -> {
+            // interface dyal sign up ghatmshi w ghatla3 dyal login
+            setVisible(false);
+            new Login().setVisible(true);
         });
 
         this.setVisible(true);
