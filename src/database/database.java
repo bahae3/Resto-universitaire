62c5b15afe7e717d04793b8f860ca38b2b9f6ef3
bahae3 <bahaeddiine12@gmail.com>
2024-05-09 00:37:29 +0100

    I created a new function to check weather the user is "Persnonnel" or not

--- src/database/database.java
+++ src/database/database.java
@@ -13,13 +13,13 @@ public class database {
             // Establishing the connection
             Connection connection = DriverManager.getConnection(url, usernameDB, passwordDB);
             // Fetching data from a table
-            String users = "SELECT email, mdp FROM Users";
+            String users = "SELECT email, mdp FROM user";
             PreparedStatement preparedStatement = connection.prepareStatement(users);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 // Process each row of the result set
-                String usernameSQL = resultSet.getString("username");
-                String passwordSQL = resultSet.getString("password");
+                String usernameSQL = resultSet.getString("email");
+                String passwordSQL = resultSet.getString("mdp");
 
                 // Retrieve other columns similarly
                 if (usernameSQL.equals(userEmail) && passwordSQL.equals(userMdp)) {
@@ -32,9 +32,44 @@ public class database {
             preparedStatement.close();
             connection.close();
         } catch (SQLException e) {
+            e.printStackTrace();
             return false;
         }
         return false;
+    }
+
+    public static int isAdmin(String userEmail, String userMdp) {
+        // Connecting to database
+        String url = "jdbc:mysql://localhost:3306/resto_univ";
+        String usernameDB = "root";
+        String passwordDB = "";
+
+        try {
+            // Establishing the connection
+            Connection connection = DriverManager.getConnection(url, usernameDB, passwordDB);
+            // Fetching data from a table
+            String users = "SELECT email, mdp FROM user WHERE estPersonnel=1";
+            PreparedStatement preparedStatement = connection.prepareStatement(users);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                // Process each row of the result set
+                String usernameSQL = resultSet.getString("email");
+                String passwordSQL = resultSet.getString("mdp");
+
+                // Retrieve other columns similarly
+                if (usernameSQL.equals(userEmail) && passwordSQL.equals(userMdp)) {
+                    return 1;
+                }
+            }
+
+            // Close the connections
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            return 0;
+        }
+        return 0;
     }
 
 }
