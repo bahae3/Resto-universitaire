48fab0bac6d68b8efaa773696b7e1b274e7095fe
bahae3 <bahaeddiine12@gmail.com>
2024-05-24 16:52:41 +0100

    I made some changes, but there is a bug to debug later

--- src/clientSide/MenuClient.java
+++ src/clientSide/MenuClient.java
@@ -1,10 +1,18 @@
-package monpanier;
+package clientSide;
+
+import database.database;
+import objects.MenuObject;
+import photos.ResizableImageLabel;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
 
-public class MenuPrincipal extends JFrame {
+public class MenuClient extends JFrame {
+    ArrayList<MenuObject> menu = database.selectMenu(1);
+    ArrayList<MenuObject> panier = new ArrayList<>();
+
     JPanel panel = new JPanel() {
         @Override
         protected void paintComponent(Graphics g) {
@@ -14,12 +22,10 @@ public class MenuPrincipal extends JFrame {
         }
     };
 
-    public MenuPrincipal() {
-        setTitle("Panier");
-        setSize(700, 630);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setLocation(200, 60);
+    public MenuClient() {
+        this.setTitle("Resto universitaire - Menu");
+        this.setBounds(200, 60, 1200, 717);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         panel.setLayout(new BorderLayout());
 
@@ -50,13 +56,56 @@ public class MenuPrincipal extends JFrame {
     private JPanel createNavBar() {
         JPanel navBarPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
 
-        String[] buttonLabels = {"Plat du jour", "Entrées", "Boissons", "Desserts", "Plats Principaux", "Menus spéciaux"};
-        for (String label : buttonLabels) {
-            JButton button = new JButton(label);
-            button.setPreferredSize(new Dimension(150, 40));
-            button.setBackground(new Color(237, 210, 133));
-            navBarPanel.add(button);
-        }
+
+        JButton button1 = new JButton("Plat du jour");
+        button1.setPreferredSize(new Dimension(150, 40));
+        button1.setBackground(new Color(237, 210, 133));
+        navBarPanel.add(button1);
+
+        button1.addActionListener(evt -> {
+//            menu = database.selectMenu(2);
+        });
+
+        JButton button2 = new JButton("Entrées");
+        button2.setPreferredSize(new Dimension(150, 40));
+        button2.setBackground(new Color(237, 210, 133));
+        navBarPanel.add(button2);
+        button2.addActionListener(evt -> {
+            menu = database.selectMenu(2);
+        });
+
+        JButton button3 = new JButton("Boissons");
+        button3.setPreferredSize(new Dimension(150, 40));
+        button3.setBackground(new Color(237, 210, 133));
+        navBarPanel.add(button3);
+        button3.addActionListener(evt -> {
+            menu = database.selectMenu(1);
+        });
+
+        JButton button4 = new JButton("Desserts");
+        button4.setPreferredSize(new Dimension(150, 40));
+        button4.setBackground(new Color(237, 210, 133));
+        navBarPanel.add(button4);
+        button4.addActionListener(evt -> {
+            menu = database.selectMenu(4);
+        });
+
+        JButton button5 = new JButton("Plats Principaux");
+        button5.setPreferredSize(new Dimension(150, 40));
+        button5.setBackground(new Color(237, 210, 133));
+        navBarPanel.add(button5);
+        button5.addActionListener(evt -> {
+            menu = database.selectMenu(3);
+        });
+
+        JButton button6 = new JButton("Menus spéciaux");
+        button6.setPreferredSize(new Dimension(150, 40));
+        button6.setBackground(new Color(237, 210, 133));
+        navBarPanel.add(button6);
+        button6.addActionListener(evt -> {
+            createScrollPane();
+            menu = database.selectMenu(5);
+        });
 
         return navBarPanel;
     }
@@ -66,28 +115,30 @@ public class MenuPrincipal extends JFrame {
         listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
         listPanel.setBackground(Color.WHITE);
 
-        for (int i = 0; i < 3; i++) {
+        for (MenuObject m : menu) {
             JPanel itemPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
             itemPanel.setBackground(Color.WHITE);
             itemPanel.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 0));
 
-            JPanel squarePanel = new JPanel();
-            squarePanel.setPreferredSize(new Dimension(80, 80));
-            squarePanel.setBackground(Color.LIGHT_GRAY);
-            itemPanel.add(squarePanel);
+            JLabel quantityLabel = new JLabel(m.quantite + "x");
+            quantityLabel.setFont(new Font("Arial", Font.BOLD, 14));
+            itemPanel.add(quantityLabel, BorderLayout.WEST);
+
+            ResizableImageLabel imageLabel = new ResizableImageLabel("src/photos/" + m.nomPhoto + ".jpg", 80, 80);
+            itemPanel.add(imageLabel);
 
             JPanel labelsPanel = new JPanel(new GridLayout(2, 1));
             labelsPanel.setBackground(Color.WHITE);
             labelsPanel.setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 0));
             itemPanel.add(labelsPanel);
 
-            JLabel itemNameLabel = new JLabel("Nom Plat ");
+            JLabel itemNameLabel = new JLabel(m.nom);
             itemNameLabel.setOpaque(true);
             itemNameLabel.setFont(new Font("Arial", Font.BOLD, 16));
             itemNameLabel.setBackground(Color.WHITE);
             labelsPanel.add(itemNameLabel);
 
-            JLabel itemPriceLabel = new JLabel("$prix");
+            JLabel itemPriceLabel = new JLabel(m.prix + " MAD");
             itemPriceLabel.setOpaque(true);
             itemPriceLabel.setFont(new Font("Arial", Font.BOLD, 16));
             itemPriceLabel.setBackground(Color.WHITE);
@@ -104,6 +155,11 @@ public class MenuPrincipal extends JFrame {
             plusButton.setFont(new Font("Arial", Font.BOLD, 16));
             plusButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             buttonPanel.add(plusButton, BorderLayout.WEST);
+            plusButton.addActionListener(evt -> {
+                m.quantite++;
+                quantityLabel.setText(m.quantite + "x");
+                panier.add(m);
+            });
 
             JButton minusButton = new JButton("-");
             minusButton.setPreferredSize(new Dimension(40, 40));
@@ -112,10 +168,13 @@ public class MenuPrincipal extends JFrame {
             minusButton.setFont(new Font("Arial", Font.BOLD, 16));
             minusButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             buttonPanel.add(minusButton, BorderLayout.CENTER);
+            minusButton.addActionListener(evt -> {
+                m.quantite--;
+                quantityLabel.setText(m.quantite + "x");
+                panier.remove(m);
+            });
 
-            JLabel quantityLabel = new JLabel(1 + "x ");
-            quantityLabel.setFont(new Font("Arial", Font.BOLD, 14));
-            buttonPanel.add(quantityLabel, BorderLayout.EAST);
+
 
             itemPanel.add(buttonPanel);
 
@@ -138,25 +197,27 @@ public class MenuPrincipal extends JFrame {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 60, 40));
         buttonPanel.setBackground(new Color(237, 210, 133));
 
-        JButton PanierButton = createButton("Panier");
+        JButton panierButton = createButton("Panier");
 
         Dimension buttonSize = new Dimension(160, 40);
-        PanierButton.setPreferredSize(buttonSize);
+        panierButton.setPreferredSize(buttonSize);
 
-        buttonPanel.add(PanierButton);
+        buttonPanel.add(panierButton);
+
+        panierButton.addActionListener(evt -> {
+            setVisible(false);
+            new PanierClient(panier);
+        });
 
         return buttonPanel;
     }
 
     private JButton createButton(String text) {
         JButton button = new JButton(text);
-        button.setBackground(new Color(75, 0, 130));
+        button.setBackground(new Color(60, 160, 240));
         button.setForeground(Color.WHITE);
         button.setFont(new Font("Arial", Font.BOLD, 16));
         return button;
     }
 
-    public static void main(String[] args) {
-        new MenuPrincipal();
-    }
 }
