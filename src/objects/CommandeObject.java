2f1d91bf207fb25b2a149549957bfa7c1fc86a08
bahae3 <bahaeddiine12@gmail.com>
2024-06-03 12:58:30 +0100

    The objects are well structured

--- src/objects/CommandeObject.java
+++ src/objects/CommandeObject.java
@@ -1,15 +1,17 @@
 package objects;
 
 public class CommandeObject {
-    public int idMenu, idUser, quantite;
+    public int idCommande, idMenu, idUser, quantite, numCommande;
     public String nomMenu, etatLivraison;
     public double prix;
-    public CommandeObject(int idMenu, String nomMenu, double prix, int idUser, int quantite, String etatLivraison) {
+    public CommandeObject(int idCommande, int idMenu, String nomMenu, double prix, int idUser, int quantite, String etatLivraison, int numCommande) {
+        this.idCommande = idCommande;
         this.idMenu = idMenu;
         this.nomMenu = nomMenu;
         this.prix = prix;
         this.idUser = idUser;
         this.quantite = quantite;
         this.etatLivraison = etatLivraison;
+        this.numCommande = numCommande;
     }
 }
--- src/objects/MenuObject.java
+++ src/objects/MenuObject.java
@@ -2,13 +2,14 @@ package objects;
 
 public class MenuObject {
     public int idMenu, quantite;
-    public String nom, nomPhoto, jourMenu, etatLivraison;
+    public String nom, nomPhoto, description, jourMenu, etatLivraison;
     public double prix;
 
-    public MenuObject(int idMenu, int qtt,String nom, String nomPhoto, String jourMenu,String etatLivraison, double prix) {
+    public MenuObject(int idMenu, int qtt, String nom, String nomPhoto, String description, String jourMenu, String etatLivraison, double prix) {
         this.idMenu = idMenu;
         this.quantite = qtt;
         this.nom = nom;
+        this.description = description;
         this.nomPhoto = nomPhoto;
         this.prix = prix;
         this.jourMenu = jourMenu;
