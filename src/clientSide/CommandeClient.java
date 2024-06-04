f8cf390c12debb4e508c8fce33c0e6d31d63dd47
bahae3 <bahaeddiine12@gmail.com>
2024-06-05 00:05:12 +0100

    Layout changed

--- src/clientSide/CommandeClient.java
+++ src/clientSide/CommandeClient.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 public class CommandeClient extends JFrame {
     int idUser;
     ArrayList<CommandeObject> commande;
-    static double totalAmount;
     JPanel panel = new JPanel() {
         @Override
         protected void paintComponent(Graphics g) {
@@ -21,16 +20,9 @@ public class CommandeClient extends JFrame {
         }
     };
 
-    public CommandeClient(double totalAmount, int idUser) {
+    public CommandeClient(int idUser) {
         this.idUser = idUser;
         this.commande = database.database.selectCommande(this.idUser);
-
-        CommandeClient.totalAmount = totalAmount;
-        System.out.println("Prix total: " + totalAmount);
-        System.out.println("Size is: " + commande.size());
-        for (CommandeObject p : commande) {
-            System.out.println(p.nomMenu);
-        }
 
         setTitle("Resto universitaire - Mes Commandes");
         setSize(1200, 717);
@@ -41,8 +33,8 @@ public class CommandeClient extends JFrame {
 
         JLabel titleLabel = createTitleLabel();
         JScrollPane scrollPane = createScrollPane();
-        JPanel buttonPanel = createButtonPanel();
         JLabel confirmationLabel = createConfirmationLabel();
+        JPanel buttonPanel = createButtonPanel();
 
         JPanel centerPanel = new JPanel(new BorderLayout());
         centerPanel.add(scrollPane, BorderLayout.CENTER);
@@ -65,78 +57,31 @@ public class CommandeClient extends JFrame {
 
     private JScrollPane createScrollPane() {
         JPanel listPanel = new JPanel();
-        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
-        listPanel.setBackground(Color.WHITE);
+        listPanel.setLayout(new GridLayout(0, 2, 10, 10));
 
-
-        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-        headerPanel.setBackground(Color.WHITE);
-        JLabel articlesLabel = new JLabel("Articles");
+        JLabel articlesLabel = new JLabel("Articles", SwingConstants.CENTER);
         articlesLabel.setFont(new Font("Arial", Font.BOLD, 18));
-        articlesLabel.setBorder(BorderFactory.createEmptyBorder(0, 80, 0, 0));
-        JLabel etatLabel = new JLabel("Etat");
+        listPanel.add(articlesLabel);
+
+        JLabel etatLabel = new JLabel("Etat", SwingConstants.CENTER);
         etatLabel.setFont(new Font("Arial", Font.BOLD, 18));
-        etatLabel.setBorder(BorderFactory.createEmptyBorder(0, 250, 0, 0));
-        headerPanel.add(articlesLabel);
-        headerPanel.add(Box.createRigidArea(new Dimension(35, 0)));
-        headerPanel.add(etatLabel);
-        listPanel.add(headerPanel);
-
-
-        listPanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+        listPanel.add(etatLabel);
 
         for (CommandeObject commande : commande) {
-            JPanel itemPanel = new JPanel(new BorderLayout());
-            itemPanel.setBackground(Color.WHITE);
-            itemPanel.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 0));
+            JPanel articlePanel = createArticlePanel(commande.quantite, commande.nomMenu, commande.prix);
+            JPanel etatPanel = createEtatPanel(commande.etatLivraison);
 
-            JPanel infoPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-            infoPanel.setBackground(Color.WHITE);
+            Border border = BorderFactory.createLineBorder(Color.BLACK);
+            articlePanel.setBorder(border);
+            etatPanel.setBorder(border);
 
-            JLabel quantityLabel = new JLabel(commande.quantite + "x ");
-            quantityLabel.setFont(new Font("Arial", Font.BOLD, 14));
-            infoPanel.add(quantityLabel);
-
-            ResizableImageLabel imageLabel = new ResizableImageLabel("src/photos/" + commande.nomMenu + ".jpg", 80, 80);
-            infoPanel.add(imageLabel);
-
-            JLabel itemNameLabel = new JLabel(commande.nomMenu);
-            itemNameLabel.setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 0));
-            itemNameLabel.setBackground(Color.WHITE);
-            itemNameLabel.setOpaque(true);
-            itemNameLabel.setFont(new Font("Arial", Font.BOLD, 16));
-
-            JLabel itemPriceLabel = new JLabel(String.valueOf(commande.prix));
-            itemPriceLabel.setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 0));
-            itemPriceLabel.setBackground(Color.WHITE);
-            itemPriceLabel.setOpaque(true);
-            itemPriceLabel.setFont(new Font("Arial", Font.BOLD, 16));
-            JPanel labelsPanel = new JPanel(new GridLayout(2, 1));
-            labelsPanel.add(itemNameLabel);
-            labelsPanel.add(itemPriceLabel);
-            infoPanel.add(labelsPanel);
-
-            JPanel LivraisonStatusPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-            LivraisonStatusPanel.setBackground(Color.WHITE);
-            LivraisonStatusPanel.setPreferredSize(new Dimension(200, 20));
-
-            JLabel deliveryStatusLabel = new JLabel(commande.etatLivraison);
-
-            deliveryStatusLabel.setFont(new Font("Arial", Font.BOLD, 14));
-            LivraisonStatusPanel.add(deliveryStatusLabel);
-            infoPanel.add(Box.createRigidArea(new Dimension(120, 0)));
-            infoPanel.add(LivraisonStatusPanel);
-
-            itemPanel.add(infoPanel, BorderLayout.CENTER);
-
-            listPanel.add(itemPanel);
-            listPanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+            listPanel.add(articlePanel);
+            listPanel.add(etatPanel);
         }
 
         JScrollPane scrollPane = new JScrollPane(listPanel);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.getViewport().setBackground(Color.WHITE);
-
 
         Border border = BorderFactory.createLineBorder(new Color(219, 219, 219), 40);
         scrollPane.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createEmptyBorder(5, 5, 5, 5)));
@@ -149,34 +94,76 @@ public class CommandeClient extends JFrame {
         confirmationLabel.setFont(new Font("Arial", Font.BOLD, 16));
         confirmationLabel.setBackground(new Color(219, 219, 219));
         confirmationLabel.setOpaque(true);
+        confirmationLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         return confirmationLabel;
+    }
+
+    private JPanel createArticlePanel(int qtt, String nomMenu, double prix) {
+        JPanel articlePanel = new JPanel(new GridBagLayout());
+        articlePanel.setBackground(Color.WHITE);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.gridx = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        JLabel quantityLabel = new JLabel(qtt + "x");
+        articlePanel.add(quantityLabel, gbc);
+
+//        JPanel squarePanel = new JPanel();
+        ResizableImageLabel imageLabel = new ResizableImageLabel("src/photos/" + nomMenu + ".jpg", 80, 80);
+//        infoPanel.add(imageLabel);
+//        squarePanel.setPreferredSize(new Dimension(80, 80));
+//        squarePanel.setBackground(Color.LIGHT_GRAY);
+        gbc.gridx = 1;
+        articlePanel.add(imageLabel, gbc);
+
+        JLabel itemNameLabel = new JLabel(nomMenu);
+        itemNameLabel.setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 0));
+        itemNameLabel.setBackground(Color.WHITE);
+        itemNameLabel.setOpaque(true);
+        itemNameLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        gbc.gridx = 2;
+        articlePanel.add(itemNameLabel, gbc);
+
+        JLabel itemPriceLabel = new JLabel(prix + " MAD");
+        itemPriceLabel.setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 0));
+        itemPriceLabel.setBackground(Color.WHITE);
+        itemPriceLabel.setOpaque(true);
+        itemPriceLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        gbc.gridx = 3;
+        articlePanel.add(itemPriceLabel, gbc);
+
+        return articlePanel;
+    }
+
+    private JPanel createEtatPanel(String etatLivraison) {
+        JPanel etatPanel = new JPanel(new GridBagLayout());
+        etatPanel.setBackground(Color.WHITE);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.gridx = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        JLabel deliveryStatusLabel = new JLabel(etatLivraison);
+        deliveryStatusLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        etatPanel.add(deliveryStatusLabel, gbc);
+
+        return etatPanel;
     }
 
     private JPanel createButtonPanel() {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 60, 40));
         buttonPanel.setBackground(new Color(219, 219, 219));
 
-        JButton OuiButton = createButton("Oui");
-        JButton NonButton = createButton("Non");
+        JButton ouiButton = createButton("Oui");
+        JButton nonButton = createButton("Non");
 
         Dimension buttonSize = new Dimension(160, 40);
-        OuiButton.setPreferredSize(buttonSize);
-        NonButton.setPreferredSize(buttonSize);
+        ouiButton.setPreferredSize(buttonSize);
+        nonButton.setPreferredSize(buttonSize);
 
-        buttonPanel.add(OuiButton);
-        buttonPanel.add(NonButton);
-
-        OuiButton.addActionListener(evt -> {
-            this.setVisible(false);
-            new Remerciement(this.idUser);
-            dispose();
-        });
-
-        NonButton.addActionListener(evt -> {
-            this.setVisible(false);
-            new Reclamation(this.idUser);
-            dispose();
-        });
+        buttonPanel.add(ouiButton);
+        buttonPanel.add(nonButton);
 
         return buttonPanel;
     }
@@ -189,8 +176,7 @@ public class CommandeClient extends JFrame {
         return button;
     }
 
-
-//    public static void main(String[] args) {
-//        new CommandeClient(100, 1);
-//    }
+    public static void main(String[] args) {
+        new CommandeClient(1);
+    }
 }
